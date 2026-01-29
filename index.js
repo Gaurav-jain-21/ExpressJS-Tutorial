e49b376a2f5620cfba4d0e4c7087d9a69246f3bd
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-29 23:29:38 +0530

    this is my commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const fs = require("fs");
+console.log("Gaurav Jain");
+fs.writeFile("output.txt", "Writing File", (err) => {
+  if (err) console.log("Error occured");
+  else console.log("File written successfully");
+});
